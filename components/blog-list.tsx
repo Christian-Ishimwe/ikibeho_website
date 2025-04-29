e0bc3b93368b89927007b423e0caa4fe1e88f7de
Ishimwe Christian <88989844+Christian-Ishimwe@users.noreply.github.com>
2025-04-29 12:28:26 +0200

    Update blog-list.tsx

--- components/blog-list.tsx
+++ components/blog-list.tsx
@@ -184,7 +184,7 @@ export function BlogList() {
                   {formatDate(blog.createdAt)}
                 </span>
                 <Link
-                  href={`/blog/${blog._id}`}
+                  href={`/blog/article?id=${blog._id}`}
                   className="inline-flex items-center gap-1 text-[#1a5fb4] text-sm font-medium hover:underline"
                 >
                   Read more <ArrowRight className="h-4 w-4" />
